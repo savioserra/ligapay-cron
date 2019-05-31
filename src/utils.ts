88a50e3b6c63f1ebc4bf4fb5ee8dfb6bf2a54cba
Sávio Serra <serra.santos.s@gmail.com>
2019-05-31 01:20:01 -0300

    Kubernetes CronJob

--- src/utils.ts
+++ src/utils.ts
@@ -1,3 +1,3 @@
-export const endpoint = `${process.env.PRISMA_ENDPOINT}/${process.env.APP_NAME}/${process.env.STAGE}`;
+export const endpoint = `${process.env.PRISMA_SERVER_ENDPOINT}/${process.env.APP_NAME}/${process.env.STAGE}`;
 
 export const appSecret = process.env.APP_SECRET;
